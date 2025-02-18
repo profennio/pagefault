f44d6547b533c8161b3e098307e7d478ce836366
profennio <147841508+profennio@users.noreply.github.com>
2025-02-18 10:38:08 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ let pageFault = 0
 
 
 
-for (let i = 0; i < 10; i++)
+for (let i = 0; i < 5; i++)
   Pages.push(i)
 let numpages = Pages.length
-for (let i = 0; i < 30; i++) {
+for (let i = 0; i < 20; i++) {
   let rndpag = Math.floor(Math.random() * numpages);
   Strrif.push(rndpag)
 
@@ -19,45 +19,53 @@ console.log(Strrif)
 
 
 Strrif.forEach(function (s, idx) {
-
+  console.log("arriva", s)
   if (!Frames.includes(s)) {
     console.log("!!!!!Pagefault")
+    pageFault++
+  
+       
     if (Frames.length < MaxFrames)
-      Frames.push(s)
+    { Frames.push(s)
+     
+   }
     else {
-      let vittima = 1000
-      let Firstrif=ottimo(0)
-      console.log(8888)
-      console.log(Firstrif)
-      Frames.forEach((f) => {if(Firstrif[f]>Firstrif[vittima]) vittima=f });
-    //  Frames.forEach((f) =>if(Firstrif[f]>Firstrif[vittima] vittima=f  );
-      
-      Frames[vittima] = s
-    }
 
-    console.log(Frames,999)
+      console.log("Frames")
+      console.log(JSON.stringify(Frames))
+      let vittima = 0
+      let Firstrif = ottimo(idx)
+     
+
+      Frames.forEach((f,idx) => {  if (Firstrif[f] > Firstrif[vittima]) vittima = idx });
+   
+      Frames[[vittima]] = s
+      console.log("Frames")
+  console.log(JSON.stringify(Frames))
+      console.log("______")
+
+    }
+  }
+  else
+    console.log("cegia")
+
+  })
+
+  console.log(pageFault, "PageFaults totali")
+  console.log(JSON.stringify(Frames), "stato finale")
+  //ottimo(0)
+  function ottimo(posto) {
+
+    let Firstrif = [];
+    let _Strrif = Strrif.slice(posto)
+    //console.log(_Strrif)
+    Pages.forEach((p) => Firstrif.push(1000));
+
+    Pages.forEach((p) => _Strrif.forEach((element, idx) => { if (element == p) if (Firstrif[p] == 1000) Firstrif[p] = idx }));
+
+    //console.log(Firstrif, 4)
+
+
+    return (Firstrif)
 
   }
-
-
-
-
-
-
-})
-ottimo(20)
-function ottimo(posto) {
-
-  let Firstrif = [];
-  let _Strrif=Strrif.slice(posto)
-  console.log(_Strrif)
-  Pages.forEach((p) => Firstrif.push(1000) );
-
- Pages.forEach((p) => _Strrif.forEach((element,idx) => {if(element==p)if(Firstrif[p]==1000) Firstrif[p]=idx} ));
-
-  console.log(Firstrif, 4)
-
-  
-  return  (Firstrif)
-
-}
