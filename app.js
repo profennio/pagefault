213ded94869921833793606f2d0cc410db9b3616
Ennio Gennari <ennio.gennari@marconirovereto.it>
2025-02-25 08:59:32 +0100

    max pagine

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 let Frames = []
 let Pages = []
-//var Strrif = [7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 1, 2, 0, 1, 7, 0, 1]
-var Strrif = [1, 2, 3, 4, 1, 2, 5, 1, 2, 3, 4, 5,0,2,1,2,3,4,0,5,4,3,6,7,1,2,0,3]
+var Strrif = [7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 1, 2, 0, 1, 7, 0, 1]
+//var Strrif = [1, 2, 3, 4, 1, 2, 5, 1, 2, 3, 4, 5,0,2,1,2,3,4,0,5,4,3,6,7,1,2,0,3]
 
 var MaxFrames = 3
 var NumPages = 6
 
-NumPages= new Set(Strrif).size;
+NumPages=Math.max(...Strrif)+1
+
+
+// new Set(Strrif).size;
+
+
 
 
 
